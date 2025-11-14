5a90bec1db3d372b8d55e90aefd189191ea7dea9
AULA8693 <bsull6@ocdsb.ca>
2025-11-14 13:54:13 -0500

    idk what this is meant to say

--- src/Drinks.java
+++ src/Drinks.java
@@ -1,3 +1,37 @@
 public class Drinks {
+    private String name;
+    private double price;
+    private int calories;
+    private String description;
+    private int numAvailable;
 
+    public Drinks(String n, double p, int c, String d, int a) {
+        name = n;
+        price = p;
+        calories = c;
+        description = d;
+        numAvailable = a;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getCalories() {
+        return calories;
+    }
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getNumAvailable() {
+        return numAvailable;
+    }
+
+
+    public void setNumAvailable(int num) {
+        numAvailable = num;
+    }
 }
