04428b1d8d9f82d3de2f1f3ef793d86707270d79
Austin Liu <eo123everyone@gmail.com>
2025-11-13 19:21:14 -0500

    made numAvailable a non-static variable

--- DessertsFoods.java
+++ DessertsFoods.java
@@ -1,8 +1,30 @@
 import java.util.ArrayList;
+
 package Food_truck;
 
+
 public class DessertsFoods {
+
+    public Food vanillaIceCream = new Food("Vanilla Ice Cream", 3.50, 250, "Soft-served vanilla ice cream.");
+    public Food chocolateIceCream = new Food("Chocolate Ice Cream", 3.50, 250, "Soft-served chocolate ice cream.");
+    public Food strawberryIceCream = new Food("Strawberry Ice Cream", 3.50, 250, "A scoop of mint chocolate ice cream.");
+    public Food brownie = new Food("Brownie", 2.75, 250, "Two rich chocolate brownies.");
+    public Food chocolateChipCookie = new Food("Chocolate Chip Cookie", 1.00, 200, "A classic chocolate chip cookie.");
+    public Food butterCookie = new Food("Butter Cookie", 1.00, 200, "A classic butter cookie.");
+    public Food applePie = new Food("Baked Apple Pie", 2.00, 250, "A warm turnover handpie filled with spiced apples.");
+    
     // Will display the menu.
-    public final ArrayList<String> dessertsMenu = new ArrayList<String>(); 
+    public DessertsFoods() {
+        
+    }
+
+    public void sellDessert(Food dessert) {
+        //System.out.println("You have purchased: " + dessert.getName() + " for $" + dessert.getPrice());
+        dessert.setNumAvailable(dessert.getNumAvailable() - 1);
+    }
+
+    
+
+
     
 }
--- Food.java
+++ Food.java
@@ -1,8 +1,17 @@
 public class Food {
     private String name;
     private double price;
-    private String calories;
-    public int numAvailable = 0;
+    private int calories;
+    private String description;
+    private int numAvailable;
+
+    public Food(String n, double p, String c, String d, int a) {
+        name = n;
+        price = p;
+        calories = c;
+        description = d;
+        numAvailable = a;
+    }
     
     public String getName() {
         return name;
@@ -13,6 +22,20 @@ public class Food {
     public String getCalories() {
         return calories;
     }
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getNumAvailable() {
+        return numAvailable;
+    }
+
+
+    public void setNumAvailable(int num) {
+        numAvailable = num;
+    }
+
+    
 
     
 }
