8e3f46ad4da7ae6a3bce59190480ca94f0a6ad31
Bohan <Bohan.921.zhang@gmail.com>
2025-11-14 14:37:57 -0500

    Test commit for MainsFood.java

--- src/MainsFood.java
+++ src/MainsFood.java
@@ -1,3 +1,9 @@
 public class MainsFood {
 
+    // public Food Food_name = new Food(String name, double price, int calories, String description, int avalibility)
+    public Food hamburger = new Food("Hamburger", 6.00, 800, "juicy beed patty with condiments that is between 2 buns", 10);
+    public Food cheeseburger = new Food("Cheeseburger", 5.00, 600, "juicy beef paty with only cheese and sauce between 2 buns", 10);
+    
+
+
 }
