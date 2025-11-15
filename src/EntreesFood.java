ea846cabf368bc7f1edecf657b8a12848dad9958
ShadowKnitter <eli.j.mccomas@gmail.com>
2025-11-14 21:56:13 -0500

    created and decided attributes for foods in the EntreesFood class

--- src/EntreesFood.java
+++ src/EntreesFood.java
@@ -1,3 +1,17 @@
 public class EntreesFood {
 
+    public Food chips = new Food("Chip Platter", 5.00, 150, "An exhuberantly delicate crisp held in a bag, presented on a silver platter", 5);
+    public Food calamari = new Food("Calamari Box", 9.75, 110, "Squid in a box.", 5);
+    public Food scallops = new Food("Scallops bag", 14.25, 137, "...its literally just scallops... in a bag...", 5);
+    public Food fries = new Food("Fries", 5.00, 150, "This perfect blend of salt and pomme de terre creates a simple yet exhilerating taste.", 5);
+    public Food fryes = new Food("Fryes", 5.00, 150, "Put the fryes in the bag and eat a family of eels!", 5);
+    public Food macNCheese = new Food("Fromage Pasta ", 3.00, 175, "An exhuberantly delicate crisp held in a bag, presented on a silver platter", 5);
+    public Food chickenNuggets = new Food("Chicken Morsels", 2.50, 200, "A decorated display of of chicken claimed as masterPIECES.", 5);
+    public Food shrimp = new Food("Fried shrimp", 7.50, 100, "smol shrimpies.", 5);
+    
+
+    public EntreesFood(){
+
+    }
+
 }
