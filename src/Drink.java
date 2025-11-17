f79fbbe5814a6f902459cab44a804ccbc122d584
AULA8693 <bsull6@ocdsb.ca>
2025-11-17 12:47:52 -0500

    Change Drinks to Foods

--- src/Drink.java
+++ src/Drink.java
@@ -4,22 +4,26 @@ public class Drink {
     private int calories;
     private String description;
     private int numAvailable;
+    public Food redGlass;
+    public Food redBottle;
+    public Food whiteGlass;
+    public Food whiteBottle;
+    public Food citrusSplash;
+    public Food berryBreeze;
+    public Food pinaColada;
+    public Food tequila;
 
-    public Drink(String n, double p, int c, String d, int a) {
-        name = n;
-        price = p;
-        calories = c;
-        description = d;
-        numAvailable = a;
+    public Drink(){
+    redGlass = new Food("Red Wineglass", 13.50, 133, "A Glass of Merlot", 20);
+    redBottle = new Food("Red Winebottle", 29.99, 295,"A Full Bottle of Merlot for you to take home", 10);
+    whiteGlass = new Food("White Wineglass", 9.99, 120, "A Glass of Chardonnay", 20);
+    whiteBottle = new Food("White Winebottle", 22.18, 266, "A Full Bottle of Chardonnay for you to take home", 10);
+    citrusSplash = new Food("Citrus Splash", 9.50, 50, "A sweet, refreshing drink made from mixed citrus juices like orange, tangerine, grapefruit, and lime.", 20);
+    berryBreeze = new Food("Berry Breeze", 2.50, 70, "A fruity, refreshing drink made from a blend of mixed berries like strawberry, blueberry, and raspberry.", 20);
+    pinaColada = new Food("Piña Colada", 3.00, 120, "A sweet, creamy drink made with pineapple and coconut.", 20);
+    tequila = new Food("Tequila", 7.50, 330, "A strong, clear spirit made from the blue agave plant in Mexico.", 10);
     }
-    public Drink redGlass = new Drink("Red Wineglass", 13.50, 133, "A Glass of Merlot", 20);
-    public Drink redBottle = new Drink("Red Winebottle", 29.99, 295,"A Full Bottle of Merlot for you to take home", 10);
-    public Drink whiteGlass = new Drink("White Wineglass", 9.99, 120, "A Glass of Chardonnay", 20);
-    public Drink WhiteBottle = new Drink("White Winebottle", 22.18, 266, "A Full Bottle of Chardonnay for you to take home", 10);
-    public Drink citrusSplash = new Drink("Citrus Splash", 9.50, 50, "A sweet, refreshing drink made from mixed citrus juices like orange, tangerine, grapefruit, and lime.", 20);
-    public Drink berryBreeze = new Drink("Berry Breeze", 2.50, 70, "A fruity, refreshing drink made from a blend of mixed berries like strawberry, blueberry, and raspberry.", 20);
-    public Drink pinaColada = new Drink("Piña Colada", 3.00, 120, "A sweet, creamy drink made with pineapple and coconut.", 20);
-    public Drink tequila = new Drink("Tequila", 7.50, 330, "A strong, clear spirit made from the blue agave plant in Mexico.", 10);
+
     public String getName() {
         return name;
     }
