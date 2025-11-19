76103de8427586076533917f9a4fcaf3478bd189
Austin Liu <eo123everyone@gmail.com>
2025-11-19 13:20:59 -0500

    Added bgMusic as File in src

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.sound.sampled.*;
+
 public class Main {
-    public static void main(String[] args) {
-        new Display();
+
+    
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        Display screen = new Display();
+
+        //File bgMusic = new File(screen.getClass().getResource("Banana Milkshake.wav"));
+        File bgMusic = new File("src/Banana Milkshake.wav");
+        AudioInputStream bgStream = AudioSystem.getAudioInputStream(bgMusic);
+
+        try {
+            Clip bgClip = AudioSystem.getClip();
+            bgClip.open(bgStream);
+            bgClip.loop(-1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
 }
