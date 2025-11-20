8781685b62614caf421c3ba5ce678ae5caad15f1
ShadowKnitter <eli.j.mccomas@gmail.com>
2025-11-20 14:37:54 -0500

    Frys, fries, and fried shrimp(and others)

--- src/EntreesFood.java
+++ src/EntreesFood.java
@@ -17,12 +17,12 @@ public class EntreesFood {
     public EntreesFood(){
         chips = new Food("Chip Platter", 5.00, 150, "Exhuberantly delicate crisps held in a bag, presented on a silver platter", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/Chip Platter.png")))));
         calamari = new Food("Calamari Box", 9.75, 110, "Squid in a box.", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/Calamari.png")))));
-        scallops = new Food("Scallops bag", 14.25, 140, "...its literally just scallops... in a bag...", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/name.png")))));
-        fries = new Food("Fries", 5.00, 150, "This perfect blend of salt and pomme de terre creates a simple yet exhilerating taste.", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/name.png")))));
-        frys = new Food("Frys", 7.00, 100, "Put the frys in the bag and eat a family of young eels!", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/name.png")))));
-        macNCheese = new Food("Fromage Pasta ", 3.00, 175, "A bowl of indubitubly delicious, cheesy, and creamy pasta, with a soft texture perfect for people of all ages", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/name.png")))));
-        chickenNuggets = new Food("Chicken Bits", 2.50, 200, "A decorated display of of chicken claimed as masterPIECES.", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/name.png")))));
-        shrimp = new Food("Fried shrimp", 7.50, 100, "smol shrimpies.", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/name.png")))));
+        scallops = new Food("Scallops bag", 14.25, 140, "...its literally just scallops... in a bag...", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/Scallops.png")))));
+        fries = new Food("Fries", 5.00, 150, "This perfect blend of salt and pomme de terre creates a simple yet exhilerating taste.", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/fries.png")))));
+        frys = new Food("Frys", 7.00, 100, "Put the frys in the bag and eat a family of young eels!", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/Frys.png")))));
+        macNCheese = new Food("Fromage Pasta ", 3.00, 175, "A bowl of indubitubly delicious, cheesy, and creamy pasta, with a soft texture perfect for people of all ages", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/MacAndCheese.png")))));
+        chickenNuggets = new Food("Chicken Bits", 2.50, 200, "A decorated display of of chicken claimed as masterPIECES.", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/ChickenNugget.png")))));
+        shrimp = new Food("Fried shrimp", 7.50, 100, "smol shrimpies.", 5, new ImageIcon((Objects.requireNonNull(getClass().getResource("img/Shrimp.png")))));
     }
 
     public void sellEntree(Food entree) {
