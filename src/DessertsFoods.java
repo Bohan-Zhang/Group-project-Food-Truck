fa4b5abfa46ecd046e55cb996dafe9a6c5e25132
Austin Liu <eo123everyone@gmail.com>
2025-11-19 22:01:56 -0500

    ice cream has scoops!

--- src/DessertsFoods.java
+++ src/DessertsFoods.java
@@ -14,8 +14,8 @@ public class DessertsFoods {
     
     // Will display the menu.
     public DessertsFoods() {
-        vanillaIceCream = new Food("Vanilla Ice Cream", 3.50, 250, "Soft-served vanilla ice cream.",5,                  new ImageIcon((Objects.requireNonNull(getClass().getResource("img/VanillaIceCream.png")))));
-        chocolateIceCream = new Food("Chocolate Ice Cream", 3.50, 250, "Soft-served chocolate ice cream.",5,            new ImageIcon((Objects.requireNonNull(getClass().getResource("img/ChocolateIceCream.png")))));
+        vanillaIceCream = new Food("Vanilla Ice Cream", 3.50, 250, "A scoop vanilla ice cream.",5,                      new ImageIcon((Objects.requireNonNull(getClass().getResource("img/VanillaIceCream.png")))));
+        chocolateIceCream = new Food("Chocolate Ice Cream", 3.50, 250, "A scoop chocolate ice cream.",5,                new ImageIcon((Objects.requireNonNull(getClass().getResource("img/ChocolateIceCream.png")))));
         strawberryIceCream = new Food("Strawberry Ice Cream", 3.50, 250, "A scoop of strawberry ice cream.",5,          new ImageIcon((Objects.requireNonNull(getClass().getResource("img/StrawberryIceCream.png")))));
         brownie = new Food("Brownie", 2.75, 250, "Two rich chocolate brownies.",5,                                      new ImageIcon((Objects.requireNonNull(getClass().getResource("img/Brownies.png")))));
         chocolateChipCookie = new Food("Chocolate Chip Cookie", 1.00, 200, "A classic chocolate chip cookie.",5,        new ImageIcon((Objects.requireNonNull(getClass().getResource("img/ChocolateChipCookies.png")))));
