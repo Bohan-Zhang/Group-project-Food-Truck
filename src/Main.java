68063cdaff31a94f5676c035c70ae4755fcd4abc
ShadowKnitter <eli.j.mccomas@gmail.com>
2025-11-19 21:31:17 -0500

    replaced the music code after its death RIP

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Display display = new Display();
+
+    
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        new Display();
+
+        //File bgMusic = new File(screen.getClass().getResource("Banana Milkshake.wav"));
+        File bgMusic = new File("src/Banana Milkshake.wav");
+
+        try {
+            AudioInputStream bgStream = AudioSystem.getAudioInputStream(bgMusic);
+            Clip bgClip = AudioSystem.getClip();
+            bgClip.open(bgStream);
+            bgClip.loop(-1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
 }
